da8749407ceedc6ca9bf1463c8f5d5e2516f582c
jocemod <88583380+jocemod@users.noreply.github.com>
2025-07-10 11:29:15 +0800

    Rebuilt main.py
    use requests rebuilt the app

--- src/main.py
+++ src/main.py
@@ -1,186 +1,167 @@
-import json, random, getopt, sys, time, os
+import getopt
+import os
+import random
+import sys
+import time
+import requests
+import json
+import re
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-
-from selenium.webdriver.common.by import By
-
-
-def read_config():
-    """"读取配置"""
-    with open("config.json") as json_file:
-        config = json.load(json_file)
-        return config
-
-
-def initialization_config(self):
-    """"配置文件初始化"""
-    file_path = "./config.json"  # 配置文件位置
-    default_config = """{
-  "display_windows": false, 
-  "cookie_refresh": 30, 
-  "user_name": "default", 
-  "password": "root123", 
-  "min_waiting_time": 0, 
-  "max_waiting_time": 5, 
-  "visited_user": "2", 
-  "recommend_mod": "https://www.mcmod.cn/class/5253.html", 
-  "uid": "2",
-  "max_retries": 3
-}"""  # 默认配置
-    if os.path.exists(file_path):
-        config = read_config()
-        if self == 'del':
-            os.remove(file_path)
-            with open("config.json", 'w') as json_file:
-                json_file.write(default_config)
-                json_file.close()  # 写入默认设置
-    else:
-        with open("config.json", 'w') as json_file:
-            json_file.write(default_config)
-            json_file.close()
-            sys.exit()  # 生成配置文件后关闭
-    return config
-
-
-# 重试装饰器
-def retry():
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            for i in range(max_retries):
-                try:
-                    return func(*args, **kwargs)
-                except Exception as e:
-                    print(f"第{i+1}次尝试失败: {e}睡眠" + str(max_waiting_time) + "秒")
-                    time.sleep(min_waiting_time)
-            print("重试失败...")
-            sys.exit(1)
-        return wrapper
-    return decorator
-
-
-def random_delay():
-    """随机延时函数，用来模拟动作比较快的点击操作"""
-    delay = random.uniform(float(min_waiting_time), float(max_waiting_time))
-    time.sleep(delay)
+file_path = "./config.json"  # 配置文件位置
+config = {
+    'username': 'root',
+    'password': 'root1234',
+    'Visited_id': '2',
+    'pushed_mod_id': '2',
+    'pushed_modpack_id': '1',
+    'random_time_interval': '1, 10',
+    'max_retries': 5,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+}
+header = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Host': 'www.mcmod.cn',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows",
+}
 
 
 def main(argv):
-    globals().update(initialization_config('None'))
+    """主函数"""
+    if os.path.exists(file_path):
+        global config
+        global header
+        with open("config.json") as json_file:
+            config = json.load(json_file)
+        header.update({'user-agent': config['user-agent']})
+    else:
+        with open("config.json", 'w') as json_file:
+            json_file.write(json.dumps(config, indent=""))
+            json_file.close()
+            sys.exit()  # 生成配置文件后关闭
     try:
         opts, args = getopt.getopt(argv, "h", ["get_cookies", "create_config", "help"])
         if not opts:
-            if os.path.exists('./cookies.json'):
-                register()
+            if os.path.exists('./Cookie'):
+                with open("Cookie") as cookie_file:
+                    data = json.loads(cookie_file.read())
+                    header.update({
+                        'Cookie': data['Cookie']
+                    })
+                    push()
+                    user_check_in(data=data['nCenterID'])
+                    view()
             else:
-                get_cookie()
+                login()
         else:
             for opt, arg in opts:
                 if opt == '-h':
                     print('--get_cookies    获取cookies\n--create_config  创建配置文件\n-h               显示命令参数')
                     sys.exit()
                 elif opt in "--get_cookies":
-                    get_cookie()
+                    login()
                 elif opt in "--create_config":
-                    initialization_config('del')
+                    os.remove(file_path)
+                    with open("config.json", 'w') as json_file:
+                        json_file.write(json.dumps(config))
+                        json_file.close()  # 写入默认设置
+                        sys.exit(1)
     except getopt.GetoptError:
         print('--get_cookies    获取cookies\n--create_config  创建配置文件\n-h               显示命令参数')  # 指令参数
 
 
-#  每日任务的函数
-@retry()
-def register():
-    if time.time() - os.path.getmtime('./cookies.json') >= cookie_refresh * 24 * 60 * 60:
-        get_cookie()  # cookie定期更新
-    service = Service()
-    options = Options()
-
-    options.add_argument("--start-maximized")  # 启动时最大化窗口
-    options.add_argument("--disable-blink-features=AutomationControlled")  # 使浏览器不显示自动化控制的信息
-    # options.add_argument("--disable-gpu")  # 禁用GPU硬件加速
-    if not display_windows:
-        options.add_argument('--headless')  # 设置为无头
-    options.add_argument("--disable-infobars")  # 隐藏信息栏
-    options.add_argument("--disable-extensions")  # 禁用所有扩展程序
-    options.add_argument("--disable-popup-blocking")  # 禁用弹出窗口拦截
-    options.add_argument("--no-sandbox")  # 关闭沙盒模式（提高性能）
-    options.add_argument("--disable-dev-shm-usage")  # 使用/dev/shm分区以避免共享内存问题
-    options.page_load_strategy = 'normal'
-
-    # 初始化 WebDriver，并传入 ChromeDriver Service
-    driver = webdriver.Edge(service=service, options=options)
-
-    with open("cookies.json", "r") as file:  # 读取cookie文件
-        cookies = json.load(file)
-
-    driver.get("https://mcmod.cn")
-
-    for cookie in cookies:  # 注入cookie
-        driver.add_cookie(cookie)
-    random_delay()
-    del cookie, cookies, file
-    try:
-        driver.get("https://center.mcmod.cn/"+str(uid)+"/#/task/")
-        random_delay()
-        driver.find_element(by=By.ID, value='task-check-in').click()  # 签到
-        random_delay()
-        driver.get(recommend_mod)
-        random_delay()
-        try:
-            driver.find_element(by=By.CLASS_NAME, value='fc-button-label').click()
-        except:
-            pass
-        try:
-            driver.find_element(by=By.ID, value='dismiss-button').click()
-        except:
-            pass
-        driver.find_element(by=By.CLASS_NAME, value='push').click()  # 推荐
-        random_delay()
-        driver.get("https://center.mcmod.cn/"+visited_user+"/")  # 窜门
-        random_delay()
-    finally:
-        driver.close()
+def random_delay(self: list | None):
+    """随机延时函数，用来模拟动作比较快的点击操作"""
+    min_waiting_time, max_waiting_time = str(config['random_time_interval']).split(',')
+    if type(self) is list:
+        min_waiting_time = self[0]
+        max_waiting_time = self[1]
+    else:
+        pass
+    sleep_time = random.uniform(float(min_waiting_time), float(max_waiting_time))
+    time.sleep(sleep_time)
+    return sleep_time
 
 
-def get_cookie():
-    options = Options()
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    if not display_windows:
-        options.add_argument('--headless')  # 设置为无头
-    driver = webdriver.Chrome(service=Service(), options=options)
+def retry():
+    """重试函数"""
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for i in range(config['max_retries']):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"第{i+1}次尝试失败: {e}睡眠" + str(random_delay(None)) + "秒")
+            print("重试失败...")
+            sys.exit(0)
+        return wrapper
+    return decorator
 
-    try:
-        os.remove('cookies.json')
-    finally:
-        for i in range(max_retries):
-            try:
-                driver.get("https://www.mcmod.cn/login/")
-                random_delay()
-                driver.find_element(by=By.ID, value='login-username').send_keys(user_name)
-                random_delay()
-                driver.find_element(by=By.ID, value='login-password').send_keys(password)
-                random_delay()
-                driver.find_element(by=By.ID, value='login-remember').click()
-                random_delay()
-                driver.find_element(by=By.ID, value='login-action-btn').click()
-                random_delay()
-                driver.refresh()
-                random_delay()
-                driver.get("https://www.mcmod.cn")
-                random_delay()
-                # 获取登录后的 cookie
-                cookies = driver.get_cookies()
-                driver.quit()
-                # 将 cookie 保存到文件
-                with open("cookies.json", "w") as file:
-                    json.dump(cookies, file)
-                return
-            except Exception as e:
-                print(f"第{i+1}次尝试失败: {e}睡眠"+str(max_waiting_time)+"秒")
-                time.sleep(min_waiting_time)
 
-        sys.exit(1)
+def login():
+    """登陆函数,生成cookie"""
+    header.update({
+        'Origin': 'https://www.mcmod.cn',
+        'Referer': 'https://www.mcmod.cn/login/',
+    })
+    data = ("data=%7B%22username%22%3A%22" + config['username'] + "%22%2C%22password%22%3A%22"
+            + config['password'] + "%22%2C%22remember%22%3A1%2C%22captcha%22%3A%22%22%7D")
+    dologin = requests.post(url='https://www.mcmod.cn/action/doLogin/', headers=header, data=data)
+    del data
+    with open("Cookie", 'w') as cookie_file:
+        match = re.search(r"MCMOD_SEED=([\w\d]+);.*_uuid=([\w\d-]+);", str(dologin.headers))
+        header.update({
+            'Cookie': f"MCMOD_SEED={match.group(1)}" + ";" + f"_uuid={match.group(2)}",
+            'Origin': '',
+        })
+        dologin = requests.get(url='https://www.mcmod.cn/login/', headers=header)
+        match2 = re.search(r"/(\d+)/$", dologin.url)
+        content = {
+            "Cookie": f"MCMOD_SEED={match.group(1)}" + ";" + f"_uuid={match.group(2)}",
+            "nCenterID": f"{match2.group(1)}",
+        }
+        cookie_file.write(json.dumps(content, indent=""))
+        cookie_file.close()  # 写入cookie
+
+
+def user_check_in(data):
+    """签到函数"""
+    header.update({
+        'Host': 'center.mcmod.cn',
+        'Origin': 'https://center.mcmod.cn',
+        'Referer': 'https://center.mcmod.cn/205548/',
+    })
+    dousercheckin = requests.post(url=f'https://center.mcmod.cn/action/doUserCheckIn/', headers=header, data=f'nCenterID={data}')
+    print(dousercheckin.content)
+
+
+def view():
+    header.update({
+        'Origin': 'https://center.mcmod.cn',
+        'Referer': 'https://center.mcmod.cn/205548/',
+    })
+    view = requests.get(url=fr'https://center.mcmod.cn/{config['Visited_id']}/#/home/', headers=header)
+    print(view.content)
+
+def push():
+    """推荐函数"""
+    header.update({
+        'Origin': 'https://www.mcmod.cn',
+        'Referer': 'https://www.mcmod.cn/class/10065.html',
+    })
+    doclass = requests.post(url=f'https://www.mcmod.cn/action/doClass/',
+                            data=f"data=%7B%22todo%22%3A%22push%22%2C%22cid%22%3A{config['pushed_mod_id']}%7D",
+                            headers=header)
+    print(doclass.content)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
